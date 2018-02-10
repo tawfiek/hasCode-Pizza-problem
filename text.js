bb922c3b354e7afc8d94ff1bd83232a77059ee21
Your Name <you@example.com>
2018-02-11 01:00:18 +0200

    solve eslint problems

--- text.js
+++ text.js
@@ -1,18 +1,18 @@
-window.onload = function() {
-		var fileInput = document.getElementById('fileInput');
-		var fileDisplayArea = document.getElementById('fileDisplayArea');
+/*eslint no-unused-vars: "error"*/
+/*eslint no-unused-vars: ["error", { "args": "none" }]*/
+/*global window , document,FileReader*/
 
-		fileInput.addEventListener('change', function(e) {
-			var file = fileInput.files[0];
-
-				var reader = new FileReader();
-				console.log(file.type);
-		
-				reader.onload = function(e) {
-					fileDisplayArea.innerText = reader.result;
-				}
-				
-				reader.readAsText(file);	
-		
-		});
-}
+window.onload = function () {
+    "use strict";
+    var fileInput = document.getElementById('fileInput'),
+        fileDisplayArea = document.getElementById('fileDisplayArea');
+    fileInput.addEventListener('change', function (e) {
+        var file = fileInput.files[0],
+            reader = new FileReader();
+        //console.log(file.type);
+		reader.onload = function (e) {
+            fileDisplayArea.innerText = reader.result;
+        };
+        reader.readAsText(file);
+    });
+};

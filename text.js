c25310085c5d89d36cca0df773c6ff1b27bbe4d2
Your Name <you@example.com>
2018-02-13 03:47:56 +0200

    resolve conflict files

--- text.js
+++ text.js
@@ -13,11 +13,8 @@ window.onload = function () {
 			let text = reader.result;
 			fileDisplayArea.innerText = reader.result;
 			//seprate string in arrays 
-			seprateString(reader.result);
-			console.log(window.allArrays);
-			console.log(window.condtions);
-			
-			
+			seprateString(text);
+			console.log(solveByRowZigiag(0,0, 5));
         };
 		reader.readAsText(file);
 		
@@ -44,10 +41,116 @@ function seprateString (text){
 	minNoOfCells : 2*allArrays[0][4]
 	}
 	this.allArrays = allArrays.slice(1);
-	 
 	return this.allArrays ; 
 }
 
-function solveByRows(arr,l,h){ 
-	console.log();
+function solveByRows(startRow ,startCol, noOfCells){ 
+	var noOfT = 0;
+	var noOfM = 0;
+	var start; 
+	var end; 
+	var currentNo = 0 ;
+	
+
+	for ( index in this.allArrays[startRow]){
+		if (currentNo < noOfCells){
+			
+			if (this.allArrays[startRow][startCol + Number(index) ] == "T") noOfT++ ; 
+			if (this.allArrays[startRow][startCol + Number(index) ] == "M") noOfM++ ;
+			currentNo ++
+		} else break;
+	}
+	return {
+		noOfT ,
+		noOfM ,
+		endcol : startCol + Number(index)-1,
+		endRow : startRow
+	}	
+}
+
+function solveByCols(startRow ,startCol, noOfCells) {
+	var noOfT = 0;
+	var noOfM = 0;
+	var start; 
+	var end; 
+	var currentNo = 0 ;
+	for ( index in this.allArrays) {
+		if (currentNo < noOfCells){
+			if (this.allArrays[startRow + Number(index)][startCol] == "T") noOfT++ ;
+			if (this.allArrays[startRow + Number(index)][startCol] == "M") noOfM++ ;
+			currentNo ++
+		}else break;
+	}
+	return {
+		noOfT ,
+		noOfM ,
+		endcol : startCol,
+		endRow : startRow + Number(index)-1
+	}
+}
+
+function solveByRowZigiagsolveByColZigiag (startRow ,startCol, noOfCells){
+	var noOfT = 0;
+	var noOfM = 0;
+	var start; 
+	var end;
+	var currentNo = 0;
+	var cells = []
+
+	for (index in this.allArrays){
+		if (currentNo < noOfCells){
+			cells.push(this.allArrays[ Number(index) + startRow ][startCol]);
+			cells.push(this.allArrays[ Number(index) + startRow ][ startCol + 1 ]);
+			currentNo += 2;
+		}else break;
+	}
+	
+	if (cells.length > noOfCells) {
+		cells.pop();
+		cells.pop();
+	}
+
+	for (let cell of cells ){ 
+		if (cell === 'T') noOfT++
+		if (cell === 'M') noOfM++
+	}
+	return {
+		noOfT ,
+		noOfM ,
+		endcol : startCol+1,
+		endRow : startRow + (cells.length/2) -1 
+	}
+}
+
+function solveByRowZigiag (startRow ,startCol, noOfCells){
+	var noOfT = 0;
+	var noOfM = 0;
+	var start; 
+	var end;
+	var currentNo = 0;
+	var cells = []
+
+	for (index in this.allArrays){
+		if (currentNo < noOfCells){
+			cells.push(this.allArrays[startRow][ Number(index) + startCol ]);
+			cells.push(this.allArrays[  startRow + 1 ][ Number(index) +startCol ]);
+			currentNo += 2;
+		}else break;
+	}
+	
+	if (cells.length > noOfCells) {
+		cells.pop();
+		cells.pop();
+	}
+
+	for (let cell of cells ){ 
+		if (cell === 'T') noOfT++
+		if (cell === 'M') noOfM++
+	}
+	return {
+		noOfT ,
+		noOfM ,
+		endcol : startCol + (cells.length/2) -1, 
+		endRow : startRow + 1 
+	}
 }
